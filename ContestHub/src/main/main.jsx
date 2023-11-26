fca10882306cc085c1dfc51eeaadc8c59f530bf2
wpseemol <wpseemol@gmail.com>
2023-11-26 16:23:11 +0600

    login page created use unic desigine

--- ContestHub/src/main/main.jsx
+++ ContestHub/src/main/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer } from 'react-toastify';
 
-import '../main/index.css';
 import router from '../router/router';
+
+import '../main/index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
--- ContestHub/src/pages/Login/Login.jsx
+++ ContestHub/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import PrimaryBtn from '../../components/PrimaryBtn/PrimaryBtn';
+import Checkbox from '../../components/Checkbox/Checkbox';
 
 const Login = () => {
     const googleLoginLoading = false;
@@ -23,8 +23,8 @@ const Login = () => {
                 <title>ContestHub | Login</title>
             </Helmet>
             <div className="text-neutral-700">
-                <div className="grid grid-cols-4">
-                    <div className="col-span-1 p-14">
+                <div className="">
+                    <div className="fixed md:w-[29rem] w-screen h-screen top-0 left-0 p-14 px-2 sm:px-14 z-20">
                         <div className="">
                             <h2 className="text-3xl font-semibold">
                                 Let’s get started
@@ -32,7 +32,7 @@ const Login = () => {
                             <p className="mt-4">
                                 Use your email address or Password.
                             </p>
-
+                            {/* google login */}
                             <div
                                 // onClick={handelGoogleLogin}
                                 className="flex my-4 justify-center gap-5 w-full py-2 rounded-full border border-black/30 hover:bg-primaryColor/80 hover:text-white duration-200">
@@ -54,9 +54,66 @@ const Login = () => {
                                 <span>or</span>
                                 <div className="border-b w-1/2 h-1"></div>
                             </div>
+
+                            <form>
+                                <div className="mb-4">
+                                    <label
+                                        htmlFor="loginEmail"
+                                        className="text-base font-semibold">
+                                        Email Address
+                                    </label>
+                                    <br className="my-2" />
+                                    <input
+                                        className=" outline-primaryColor/60 border py-3 rounded text-lg pl-4 w-full mt-1"
+                                        type="email"
+                                        name="loginEmail"
+                                        id="emailId"
+                                        placeholder="johndoe@gmail.com"
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label
+                                        htmlFor="loginPassword"
+                                        className="text-base font-semibold">
+                                        Password
+                                    </label>
+                                    <br />
+                                    <input
+                                        className=" outline-primaryColor/60 border py-3 rounded text-lg pl-4 w-full mt-1"
+                                        type="password"
+                                        name="loginPassword"
+                                        id="passwordId"
+                                        placeholder="Please enter your password"
+                                        required
+                                    />
+                                </div>
+                                <div className="flex items-center justify-between mb-3 relative">
+                                    <div>
+                                        <Checkbox />
+                                    </div>
+                                    <div className="text-primaryColor text-base ">
+                                        <p>Forgot your password</p>
+                                    </div>
+                                </div>
+                                <div className="mt-8 mb-4">
+                                    <PrimaryBtn>
+                                        <span className="uppercase">Login</span>
+                                    </PrimaryBtn>
+                                </div>
+                                <div>
+                                    <p>
+                                        Don’t have an account?{' '}
+                                        <span className="text-primaryColor">
+                                            Sign Up
+                                        </span>
+                                    </p>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                    <div className="col-span-3 min-h-screen bg-[#f2f2f2]">
+
+                    <div className="ml-auto hidden md:block w-[calc(100%-29rem)] h-screen bg-[#f2f2f2]">
                         <div className="flex items-center justify-center h-full">
                             <div className="w-2/3">
                                 <Slider {...settings}>
